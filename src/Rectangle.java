1e74e660cc0931b0a7eae2e864dff5a35375b5d8
Ryo-N-0106 <ntkn717416@gmail.com>
2025-05-18 10:32:52 +0700

    BT Rectangle_Sửa

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -7,23 +7,23 @@ public class Rectangle {
         this.height = height;
     }
 
-    public void getArea() {
-        double area = this.width * this.height;
-        System.out.printf("Diện tích hình chữ nhật: " + area + "\n");
+    public double getArea() {
+        double area;
+        return area = this.width * this.height;
     }
 
-    public void getPerimeter() {
-        double perimeter = (this.width + this.height) * 2;
-        System.out.printf("Chu vi hình chữ nhật: " + perimeter + "\n");
+    public double getPerimeter() {
+        double perimeter;
+        return perimeter = (this.width + this.height) * 2;
     }
 
     public static void main(String[] args) {
         Rectangle hcn1 = new Rectangle(5,6);
         Rectangle hcn2 = new Rectangle(7.5,4);
 
-        hcn1.getArea();
-        hcn1.getPerimeter();
-        hcn2.getArea();
-        hcn2.getPerimeter();
+        System.out.printf("Diện tích hình chữ nhật 1: " + hcn1.getArea() + "\n");
+        System.out.printf("Diện tích hình chữ nhật 2: " + hcn2.getArea() + "\n");
+        System.out.printf("Chu vi hình chữ nhật 1: " + hcn1.getPerimeter() + "\n");
+        System.out.printf("Chu vi hình chữ nhật 2: " + hcn2.getPerimeter() + "\n");
     }
 }
